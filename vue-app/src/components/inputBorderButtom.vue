6ef70d3cfb3f036b489cb7f55b3ef7b0fb947ff2
Jarvis <13g7895123@gmail.com>
2023-05-28 23:40:53 +0800

    test vue

---
+++ vue-app/src/components/inputBorderButtom.vue
@@ -0,0 +1,9 @@
+<template>
+    <input class='text-xl pl-1 pb-1 outline-none border-b-2 border-slate-400 mt-10' placeholder="{{ placeholder }}">
+</template>
+
+<script setup>
+defineProps({
+  placeholder: String,
+})
+</script>
--- vue-app/src/pages/LoginPage.vue
+++ vue-app/src/pages/LoginPage.vue
@@ -10,6 +10,13 @@ const loginBoxMt = ref('')
 
 loginBoxMt.value = isLogin ? 'mt-[10dvh]' : 'mt-0'
 
+const account = ref('')
+const password = ref('')
+
+const loginSubmit = () => {
+    console.log(`${account}/${password}`);
+}
+
 </script>
 
 <template>
@@ -19,13 +26,13 @@ loginBoxMt.value = isLogin ? 'mt-[10dvh]' : 'mt-0'
         </div>
         <div class=' w-64 h-full border-l-2 border-slate-300 flex flex-col items-center px-3'>
             <div class='text-2xl font-bold mt-10'>會員登入</div>
-            <input name='account' class='text-xl pl-1 pb-1 outline-none border-b-2 border-slate-400 mt-10' placeholder='帳號'>
+            <input v-model="account" class='text-xl text-slate-700 pl-1 pb-1 outline-none border-b-2 border-slate-400 mt-10' placeholder='帳號'>
             <div class='relative flex'>
-                <input name='pwd' class='text-xl pl-1 pb-1 outline-none border-b-2 border-slate-400 mt-5' placeholder='密碼' type='password'>
+                <input v-model="password" class='text-xl pl-1 pb-1 outline-none border-b-2 border-slate-400 mt-5' placeholder='密碼' type='password'>
                 <img src='../assets/hide.png' id='showPwd' class="w-6 h-6 absolute right-1 bottom-2 animate-3 cursor-pointer">
             </div>
             <div class='w-full mt-10 flex justify-center text-justify'>
-                <div id='btn_submit' class=' w-32 h-auto flex justify-around items-center px-5 py-1 text-xl text-justify font-bold bg-green-300 animate-3 hover:bg-green-500 rounded-full cursor-pointer'>登入</div>
+                <div @click="loginSubmit" class=' w-32 h-auto flex justify-around items-center px-5 py-1 text-xl text-justify font-bold bg-green-300 animate-3 hover:bg-green-500 rounded-full cursor-pointer'>登入</div>
             </div>
             <div class='mt-5'>還不是會員?<a href='./register.php' class='cursor-pointer text-blue-600 animate-3 hover:font-extrabold hover:border-b-2 hover:border-blue-600 duration-100'>馬上加入!</a></div>
         </div>
