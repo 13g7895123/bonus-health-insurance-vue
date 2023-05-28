84fffa13c81c099fde28627c1d45011423c3a99c
Jarvis <13g7895123@gmail.com>
2023-05-28 22:09:08 +0800

    change sidebar and navbar show on login page

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -4,18 +4,18 @@ import Navbar from './components/Navbar.vue';
 import Sidebar from './components/Sidebar.vue';
 import NavbarRight from './components/NavbarRight.vue';
 
-// const isLogin = false
+const isLogin = false
 
 </script>
 
 <template>
   <div class="flex overflow-hidden">
     <!-- left -->
-    <Sidebar />
+    <Sidebar v-show="isLogin"/>
     <!-- right -->
     <div class="w-[calc(100%-15rem)] flex flex-col">
       <!-- navbar -->
-      <NavbarRight />
+      <NavbarRight v-show="isLogin"/>
       <!-- content -->
       <div class="h-[calc(100%-2.5rem)] flex justify-center items-center">
         <router-view/>
